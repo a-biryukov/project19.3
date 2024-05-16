0eb7246266d13e0fae322b58d4492f28fe1763e0
Alexander <a.biryukov164@gmail.com>
2024-05-16 12:18:56 +0400

    add templates

--- catalog/views.py
+++ catalog/views.py
@@ -16,3 +16,10 @@ def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
     context = {'product': product}
     return render(request, 'product_detail.html', context)
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products_list.html', context)
+
