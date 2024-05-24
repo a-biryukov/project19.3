ac2c97025376980ca80e42518c965f5761463efa
Alexander <a.biryukov164@gmail.com>
2024-05-24 20:06:44 +0400

    создал модель Blog

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    tittle = models.CharField(max_length=100, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    image = models.ImageField(upload_to='category/images', blank=True, null=True, verbose_name='Изображение')
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    view_count = models.IntegerField(default=0, verbose_name=' Просмотры')
+    slug = models.CharField(max_length=100, verbose_name='Slug', null=True, blank=True)
+
+    def __str__(self):
+        return self.tittle
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
