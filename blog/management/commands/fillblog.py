2cab709957e4fcc1a24feaa44b57e3bd70ad460d
Alexander <a.biryukov164@gmail.com>
2024-05-25 23:33:47 +0400

    написал команду для заполнения отзывов

---
+++ blog/management/commands/fillblog.py
@@ -0,0 +1,30 @@
+import json
+
+from django.core.management import BaseCommand
+from blog.models import Blog
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories():
+        with open('fixtures/blog_data.json', encoding='utf-8') as file:
+            return json.load(file)
+
+    def handle(self, *args, **options):
+
+        # Удаление старых данных
+        Blog.objects.all().delete()
+
+        # Списки для хранения объектов
+        blog_for_create = []
+
+        # Создаем объекты отзывов из фикстур
+        for blog in Command.json_read_categories():
+            category_data = blog.get('fields')
+            blog_for_create.append(
+                Blog(**category_data)
+            )
+
+        # Создаем объекты категорий в базе
+        Blog.objects.bulk_create(blog_for_create)
