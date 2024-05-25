59ae81a6d163de70e8455db16e44aae773fdde55
Alexander <a.biryukov164@gmail.com>
2024-05-26 01:17:34 +0400

    добавил счетчик просмотров, динамическое формирование slug, перенаправление после редактирования

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, connection
 
 
 class Blog(models.Model):
@@ -8,7 +8,7 @@ class Blog(models.Model):
     created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
 
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    view_count = models.IntegerField(default=0, verbose_name=' Просмотры')
+    views_count = models.IntegerField(default=0, verbose_name=' Просмотры')
     slug = models.CharField(max_length=100, verbose_name='Slug', null=True, blank=True)
 
     def __str__(self):
@@ -17,3 +17,8 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
+
+    @staticmethod
+    def truncate_table_restart_id():
+        with connection.cursor() as cursor:
+            cursor.execute('TRUNCATE TABLE blog_blog * RESTART IDENTITY')
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from pytils.translit import slugify
+
 from blog.models import Blog
 
 
 class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class BlogCreateView(CreateView):
@@ -12,15 +19,30 @@ class BlogCreateView(CreateView):
     fields = ("tittle", "text", "image")
     success_url = reverse_lazy("blog:blog_list")
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.tittle)
+            new_blog.save()
+            return super().form_valid(form)
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ("tittle", "text", "image")
-    success_url = reverse_lazy("blog:blog_list")
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
