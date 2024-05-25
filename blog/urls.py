748102f911f61113bdf71a896cf1aab7de82587e
Alexander <a.biryukov164@gmail.com>
2024-05-25 19:51:24 +0400

    реализовал создание отзыва

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogCreateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('blogs/', BlogListView.as_view(), name='blog_list'),
+    path('blog_create/', BlogCreateView.as_view(), name='blog_form'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from blog.models import Blog
 
 
 class BlogListView(ListView):
     model = Blog
 
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ("tittle", "text", "image")
+    success_url = reverse_lazy("blog:blog_list")
 
