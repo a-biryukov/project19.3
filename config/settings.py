7ea537df841a232a8e5036c4adcb6c8c5cc09bc1
Alexander <a.biryukov164@gmail.com>
2024-06-22 17:17:07 +0400

    убрал возможность менять почту в профиле

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'catalog',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
