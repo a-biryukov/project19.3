f6f72a963eb07fb856e0638b6742d71f03cb1347
Alexander <a.biryukov164@gmail.com>
2024-06-29 21:58:36 +0400

    вынес в services.py функцию добавления информации о текущей версии продукта

--- catalog/services.py
+++ catalog/services.py
@@ -1,6 +1,6 @@
 from django.core.cache import cache
 
-from catalog.models import Category
+from catalog.models import Category, Version
 from config.settings import CACHES_ENABLED
 
 
@@ -17,3 +17,12 @@ def get_categories_from_cache():
     return categories
 
 
+def add_version_info(queryset):
+    """Добавляет в объкты продуктов информацию о текущей версии"""
+    for product in queryset:
+        version = Version.objects.filter(product=product)
+        current_version = version.filter(current_version=True)
+        if current_version:
+            product.version_name = current_version.last().version_name
+            product.version_num = current_version.last().version_num
+    return queryset
